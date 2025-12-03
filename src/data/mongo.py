f6f9f252ea4fb8282c0e2b9e7596c1e16ca7ed2a
NVB20 <nivb49@gmail.com>
2025-12-03 02:12:50 +0200

    db connection

---
+++ src/data/mongo.py
@@ -0,0 +1,38 @@
+
+import streamlit as st
+from pymongo import MongoClient
+import pandas as pd
+from config.settings import MONGO_USER
+from config.settings import MONGO_PASSWORD
+from config.settings import MONGO_PORT
+
+
+# MongoDB Connection
+MONGO_URL = f"mongodb://{MONGO_USER}:{MONGO_PASSWORD}@localhost:{MONGO_PORT}/?authSource=admin"
+
+client = MongoClient(MONGO_URL)
+db = client["students_db"]
+collection = db["student_stats"]
+
+
+# Load Data
+@st.cache_data(ttl=60)
+def load_students():
+    docs = list(collection.find())
+
+    if not docs:
+        return pd.DataFrame()
+
+    for d in docs:
+        for lesson in d.get("lessons", []):
+            try:
+                lesson["practice_count"] = int(lesson["practice_count"])
+            except:
+                pass
+
+    df = pd.DataFrame(docs)
+
+    if "_id" in df.columns:
+        df = df.drop(columns=["_id"])
+
+    return df
