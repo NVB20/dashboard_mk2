289fefa343d31b4a85d398772f64b434fad0bddb
NVB20 <nivb49@gmail.com>
2025-12-03 02:12:14 +0200

    settings

---
+++ src/config/settings.py
@@ -0,0 +1,17 @@
+import os
+from dotenv import load_dotenv
+import streamlit as st
+
+# Load environment variables
+load_dotenv()
+
+MONGO_USER = os.getenv("MONGO_USER")
+MONGO_PASSWORD = os.getenv("MONGO_PASSWORD")
+MONGO_PORT = os.getenv("MONGO_PORT")
+TOTAL_LESSONS = int(os.getenv("TOTAL_LESSONS"))
+
+st.set_page_config(
+    page_title="Student Stats Dashboard",
+    layout="wide",
+    initial_sidebar_state="expanded"
+)
