7457c1914ba2b900216239605c4ba158d70e3fc3
NVB20 <nivb49@gmail.com>
2025-12-09 00:06:08 +0200

    added env vars

--- src/config/settings.py
+++ src/config/settings.py
@@ -8,7 +8,14 @@ load_dotenv()
 MONGO_USER = os.getenv("MONGO_USER")
 MONGO_PASSWORD = os.getenv("MONGO_PASSWORD")
 MONGO_PORT = os.getenv("MONGO_PORT")
+MONGO_HOST = os.getenv("MONGO_HOST")
 TOTAL_LESSONS = int(os.getenv("TOTAL_LESSONS"))
+
+print("="*60)
+print(MONGO_HOST)
+print("="*60)
+
+
 
 st.set_page_config(
     page_title="Student Stats Dashboard",
